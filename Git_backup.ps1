a9eb8d43818bfa25e6b32f4e2bd0df083334c94d
Sipubot <ddasik00@naver.com>
2019-03-02 13:55:30 +0900

    ..

---
+++ Git_backup.ps1
@@ -0,0 +1,39 @@
+﻿# github
+cd D:\__Git\CodeInvaders
+git pull https://github.com/sipubot/CodeInvaders.git master
+
+cd D:\__Git\ENV-AUTOMATION
+git pull https://github.com/sipubot/ENV-AUTOMATION.git master
+
+cd D:\__Git\HowToGetTheJob
+git pull https://github.com/sipubot/HowToGetTheJob.git master
+
+cd D:\__Git\JS-CanvasASMR
+git pull https://github.com/sipubot/JS-CanvasASMR.git master
+
+cd D:\__Git\JS-CanvasSlider
+git pull https://github.com/sipubot/JS-CanvasSlider.git master
+
+cd D:\__Git\JS-Debugger
+git pull https://github.com/sipubot/JS-Debugger.git master
+
+cd D:\__Git\JS-gettersetter
+git pull https://github.com/sipubot/JS-gettersetter.git master
+
+cd D:\__Git\JS-Le-gogh
+git pull https://github.com/sipubot/JS-Le-gogh.git master
+
+cd D:\__Git\JS-SipuBoilerPlate
+git pull https://github.com/sipubot/JS-SipuBoilerPlate.git master
+
+cd D:\__Git\MovingSnail
+git pull https://github.com/sipubot/MovingSnail.git master
+
+cd D:\__Git\rhetoric
+git pull https://github.com/sipubot/rhetoric.git master
+
+cd D:\__Git\sipubot.github.io
+git pull https://github.com/sipubot/sipubot.github.io.git master
+
+cd D:\__Git\WIKI
+git pull https://github.com/sipubot/WIKI.git master
