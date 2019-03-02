575f88f878853e431d541fb4c6f65cb627c39972
SIPU <ddasik00@naver.com>
2019-03-03 02:41:05 +0900

    Update Git_backup.ps1

--- Git_backup.ps1
+++ Git_backup.ps1
@@ -1,52 +1,65 @@
 # github
 cd D:\__Git\CodeInvaders
-git checkout HEAD^ file/to/overwrite
+git stash
 git pull https://github.com/sipubot/CodeInvaders.git master
+git stash clear
 
 cd D:\__Git\ENV-AUTOMATION
-git checkout HEAD^ file/to/overwrite
+git stash
 git pull https://github.com/sipubot/ENV-AUTOMATION.git master
+git stash clear
 
 cd D:\__Git\HowToGetTheJob
-git checkout HEAD^ file/to/overwrite
+git stash
 git pull https://github.com/sipubot/HowToGetTheJob.git master
+git stash clear
 
 cd D:\__Git\JS-CanvasASMR
-git checkout HEAD^ file/to/overwrite
+git stash
 git pull https://github.com/sipubot/JS-CanvasASMR.git master
+git stash clear
 
 cd D:\__Git\JS-CanvasSlider
-git checkout HEAD^ file/to/overwrite
+git stash
 git pull https://github.com/sipubot/JS-CanvasSlider.git master
+git stash clear
 
 cd D:\__Git\JS-Debugger
-git checkout HEAD^ file/to/overwrite
+git stash
 git pull https://github.com/sipubot/JS-Debugger.git master
+git stash clear
 
 cd D:\__Git\JS-gettersetter
-git checkout HEAD^ file/to/overwrite
+git stash
 git pull https://github.com/sipubot/JS-gettersetter.git master
+git stash clear
 
 cd D:\__Git\JS-Le-gogh
-git checkout HEAD^ file/to/overwrite
+git stash
 git pull https://github.com/sipubot/JS-Le-gogh.git master
+git stash clear
 
 cd D:\__Git\JS-SipuBoilerPlate
-git checkout HEAD^ file/to/overwrite
+git stash
 git pull https://github.com/sipubot/JS-SipuBoilerPlate.git master
+git stash clear
 
 cd D:\__Git\MovingSnail
-git checkout HEAD^ file/to/overwrite
+git stash
 git pull https://github.com/sipubot/MovingSnail.git master
+git stash clear
 
 cd D:\__Git\rhetoric
-git checkout HEAD^ file/to/overwrite
+git stash
 git pull https://github.com/sipubot/rhetoric.git master
+git stash clear
 
 cd D:\__Git\sipubot.github.io
-git checkout HEAD^ file/to/overwrite
+git stash
 git pull https://github.com/sipubot/sipubot.github.io.git master
+git stash clear
 
 cd D:\__Git\WIKI
-git checkout HEAD^ file/to/overwrite
+git stash
 git pull https://github.com/sipubot/WIKI.git master
+git stash clear
